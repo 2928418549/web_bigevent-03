621f9a621e5916de8fab29f2789eef452f310d2b
tao <2928418549@qq.com>
2020-11-27 11:16:16 +0800

    完成login

---
+++ assets/js/login.js
@@ -0,0 +1,41 @@
+$(function(){
+    $('#reg').on('click',function(){
+        $('#box-login').hide()
+        $('#box-reg').show()
+    })
+    $('#login').on('click',function(){
+        $('#box-login').show()
+        $('#box-reg').hide()
+    })
+    $('#box-reg').on('submit',function(e){
+        e.preventDefault()
+        $.ajax({
+            method:'POST',
+            url:'/api/reguser',
+            data:$(this).serialize(),
+            success:function(res){
+                if(res.status!==0){
+                    return layer.msg(res.message)
+                }
+                layer.msg('注册成功')
+                $('#login').click()
+            }
+        })
+    })
+    $('#box-login').on('submit',function(e){
+        e.preventDefault()
+        $.ajax({
+            method:'POST',
+            url:'/api/login',
+            data:$(this).serialize(),
+            success:function(res){
+                if(res.status!==0){
+                    return layer.msg(res.message)
+                }
+                layer.msg('登录成功')
+                localStorage.setItem('token',res.token)
+                location.href="/index.html"
+            }
+        })
+    })
+})
