b1876e3b63a15f8314bde0e61f5b9667e42cd31d
tao <2928418549@qq.com>
2020-11-27 14:39:25 +0800

    完成了首页制作

--- assets/js/baseAPL.js
+++ assets/js/baseAPL.js
@@ -1,5 +1,16 @@
 var baseURL = 'http:///ajax.frontend.itheima.net'
-$.ajaxPrefilter(function(option){
+$.ajaxPrefilter(function (option) {
     option.url = baseURL + option.url
-    
+    if (option.url.indexOf('/my/') !== -1) {
+        option.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    option.complete = function (res) {
+        console.log(res);
+        if (res.responseJSON.status == 1 && res.responseJSON.message == '身份认证失败！') {
+            localStorage.removeItem('token')
+            location.href = "/login.html"
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,33 @@
+$(function () {
+    getUserInfo()
+    $('#btnLogout').on('click',function(){
+        layer.confirm('确定要退出?', {icon: 3, title:'提示'}, function(index){
+            localStorage.removeItem('token')
+            location.href="/login.html"
+            layer.close(index);
+          });
+    })
+})
+function getUserInfo() {
+    $.ajax({
+        url: '/my/userinfo',
+        success: function (res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+            }
+            layer.msg('获取用户信息成功')
+            renderAvatar(res.data)
+        }
+    })
+}
+function renderAvatar(user) {
+    var name = user.nickname || user.username
+    $('#welcome').html(name)
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.user-avatar').hide()
+    }else{
+        $('.layui-nav-img').hide()
+        $('.user-avatar').html(name[0].toUpperCase()).show()
+    }
+}
